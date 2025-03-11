1d842a17082bc5c0442f414ce6f68073d25ba882
Rakesh Pavan <110276813+rakeshtadi@users.noreply.github.com>
2025-03-11 12:52:05 +0530

    operators js

---
+++ rakesh.js
@@ -0,0 +1,34 @@
+//ARITHMETIC OPERATORS
+let a = 10;
+let b = 5;
+console.log("a = ",a, "b = ", b);
+console.log("a + b = ", a+b);
+console.log("a - b = ", a-b);
+console.log("a * b = ", a*b);
+console.log("a / b = ", a/b);
+console.log("a ** b = ", a**b);
+console.log("a % b = ", a%b);
+
+//Unary operators
+a++;
+console.log("a = ", a); // Post increment
+--a;
+console.log("--a =", --a); //Pre decrement
+
+
+//Assignment operator
+let a1 = 11;
+
+a1 += 5;
+console.log("a1 = ", a1);
+
+a1 -= 5;
+console.log("a1 =",a1);
+a1 *= 5;
+console.log("a1 = ", a1);
+a1 /= 5;
+console.log("a1 = ", a1);
+a1 %= 5;
+console.log("a1 = ", a1);
+a1 **= 5;
+console.log("a1 = ",a1);
